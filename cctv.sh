1e120de39c484f3de69bdd3a35e98a85be669759
lilung <49183664+lilung@users.noreply.github.com>
2019-04-30 08:21:59 +0700

    Add files via upload

---
+++ cctv.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#
+clear
+echo "mulai menjalankan tool pedang hack cctv"
+load
+clear
+echo Selamat datang tod, Siapa nama lu ? #tulisan keluar
+read nick #membaca yang ditulis
+clear
+
+#logo_credit
+sleep 0.1
+echo "==================================="
+sleep 0.3
+echo "(====)>>>>>>>>>>>>>>>"
+echo
+sleep 0.5
+echo "TOOL PEDANG HACK CCTV"
+sleep 0.7
+echo "author: lilung"
+sleep 0.9
+echo "facebook: lilung"
+sleep 1.1
+echo "youtube: lilung project"
+sleep 1.3
+echo "==================================="
+
+echo Selamat datang $nick ":)" di tool pedang hack cctv! 
+echo 
+echo "Mulai hack cctv tod?"
+echo "y/n"
+read confirm
+if [ $confirm = "y" ]; then
+    echo "sedang coli mohon tunggu"
+    curl -s http://zlucifer.hol.es/Project_eye_of_all_seeing/index.php
+    echo " udah selesai silakan pilih dan copy ip cctv nya tod "
+    echo "lalu pastekan di crome atau browser internet"
+    echo
+    echo " jangan hack cctv untuk ngintip orang mandi goblok "
+    echo "matalu bintitan ntar"
+    echo " -lilung"
+    echo "======================================="
+else
+    echo "ada Kesalahan goblok"
+fi
